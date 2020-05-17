bc2f2c3a12c83a50df47bc08001e2ae2ca717e27
Roman Zhigalov <larkie.wistful+github@gmail.com>
2020-05-17 19:30:41 +0300

    Setup EmailsInput element and child container
    feat. support namespaced styles

--- emails-input.js
+++ emails-input.js
@@ -3,14 +3,27 @@
 var EmailsInput = (function () {
 
   var DEFAULT_OPTIONS = {
-
+    cssNamespace: null
   };
 
   return function EmailsInput(element, opts) {
 
+    function setNsClassName(className) {
+      return options.cssNamespace ? options.cssNamespace + '-' + className : className;
+    }
+
+    var container;
     var options;
 
     options = Object.assign({}, DEFAULT_OPTIONS, opts);
+
+    // Setup parent element
+    element.classList.add(setNsClassName('emails-input'));
+
+    // Create and setup child container element
+    container = document.createElement('div');
+    container.classList.add(setNsClassName('emails-input__container'));
+    element.appendChild(container);
 
     return {
       container: element,
